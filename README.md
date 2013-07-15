2c1616b5d833c76c25ba182e7def01d141deb194
Michał Zieliński <michal@zielinscy.org.pl>
2013-07-15 11:54:12 +0200

    generate ssh during installation, README

---
+++ README.md
@@ -0,0 +1,16 @@
+
+Installation
+------------
+
+1. Bootstrap gravel-package:
+
+    wget -O- -q http://repo.gravel.atomshare.net/bootstrap.sh | bash
+
+2. Install gravel-node:
+
+    gravelpkg install gravel-node
+
+3. Run shown command on master.
+4. Run:
+
+    gravelregister <master hostname>
--- setup.sh
+++ setup.sh
@@ -6,3 +6,15 @@ ln -sf $PWD/register.py /usr/local/bin/gravelregister
 
 echo "gravelnode ALL = (ALL) NOPASSWD: ALL" > /etc/sudoers.d/gravelnode
 chmod 440 /etc/sudoers.d/gravelnode
+
+GHOME=/gravel/system/gravelnode
+if [ ! -e $GHOME/.ssh/id_rsa ]; then
+    sudo -u gravelnode mkdir $GHOME/.ssh/
+    sudo -u gravelnode ssh-keygen -f $GHOME/.ssh/id_rsa -N ''
+
+    echo
+    echo "Run on master:"
+    echo
+    echo gravel register `hostname` \""$(cat $GHOME/.ssh/id_rsa.pub)"\"
+    echo
+fi
